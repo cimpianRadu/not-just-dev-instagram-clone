c5b48c56d492e357758f16c97ab3a5b2c0ebf613
cimpianRadu <cimpian.radu@yahoo.com>
2023-04-10 13:58:25 +0300

    nested deep link

--- InstagramClone/src/navigation/index.tsx
+++ InstagramClone/src/navigation/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {LinkingOptions, NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BottomTabNavigator from './BottomTabNavigator';
 import CommentsScreen from '../screens/CommentsScreen';
@@ -7,9 +7,30 @@ import {RootNavigatorParamList} from './types';
 
 const Stack = createNativeStackNavigator<RootNavigatorParamList>();
 
+const linking: LinkingOptions<RootNavigatorParamList> = {
+  prefixes: ['instagramclone://', 'https://instagramclone.com'],
+  config: {
+    initialRouteName: 'Home',
+    screens: {
+      Comments: 'comments', // instagramclone://comments
+      // instagramclone://user:123 - nested navigator mapping
+      Home: {
+        screens: {
+          HomeStack: {
+            initialRouteName: 'Feed',
+            screens: {
+              UserProfile: 'user/:userId',
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
 const Navigation = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
